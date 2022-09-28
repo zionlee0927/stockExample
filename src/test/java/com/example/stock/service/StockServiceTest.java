3671f704ebe568170ef7414410bda60fdd69fe69
zion <zionlee0927@gmail.com>
2022-09-28 12:08:14 +0900

    feat: stock decrease add

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,44 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void before() {
+        stockRepository.save(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void stock_decrease_test_verify_stock_99_when_decrease_1() {
+
+        // when
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // then
+        assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+}
